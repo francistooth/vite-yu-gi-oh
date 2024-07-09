63e8b0a5ba016bff0617c6680d7c12eb23f41809
Francesco Dente <francesco.dente2003@gmail.com>
2024-07-09 14:37:49 +0200

    1.0.2: fix:  html icon and title fixed, offset variant

--- src/store.js
+++ src/store.js
@@ -2,5 +2,5 @@ import { reactive } from "vue";
 
 export const store = reactive({
     cardList: [],
-    apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+    apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=100&offset=200",
 });
