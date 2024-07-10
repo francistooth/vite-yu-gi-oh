3732f388511daf52ee684a09b3f7cae4ca9252e6
Francesco Dente <francesco.dente2003@gmail.com>
2024-07-10 17:48:07 +0200

    1.0.4: feature: select dynamic population from API

--- src/components/SelectRace.vue
+++ src/components/SelectRace.vue
@@ -1,12 +1,31 @@
 <script>
 import { store } from '../store';
 
+// axios import
+
+import axios from 'axios';
+
 export default {
     name: 'SelectRace',
     data() {
         return {
             store,
         }
+    },
+    methods: {
+        getArch(){
+            axios.
+            get(store.archUrl)
+            .then(res => {
+                console.log(res.data);
+                store.archList = res.data;
+            }).catch(err => {
+                console.log(err);
+            });
+        }
+    },
+    mounted() {
+        this.getArch();
     },
 }
 </script>
@@ -15,13 +34,16 @@ export default {
     <div class="container mb-3">
         <label class="labels" for="raceSelect"> Filter by race:  </label>
         <select v-model="store.search" @change="$emit('search')">
-            <option value=""></option>
+            <option v-for="arch in store.archList" :value="arch.archetype_name">
+                {{ arch.archetype_name }}
+            </option>
+            <!-- <option value=""></option>
             <option value="Alien"> Alien </option>
             <option value="Infernoble Arms"> Infernoble Arms</option>
             <option value="Noble Knight"> Noble Knight </option>
             <option value="Ally of Justice"> Ally of Justice </option>
             <option value="Melodious"> Melodious </option>
-            <option value="Gagaga"> Gagaga </option>
+            <option value="Gagaga"> Gagaga </option> -->
         </select>
     </div>
 </template>
--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { reactive } from "vue";
 
 export const store = reactive({
     cardList: [],
+    archList: [],
     apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=300&offset=0",
+    archUrl: "https://db.ygoprodeck.com/api/v7/archetypes.php",
     search: ""
 });
